fff363652c33ffb450f6ca77570f1c039eb1240b
MatheusWDB <matheuswdb1703@gmail.com>
2025-05-22 20:01:10 -0300

    build: :construction: configuração inicial do projeto

---
+++ backend/src/test/java/br/com/hematsu/lance_certo/LanceCertoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hematsu.lance_certo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanceCertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ frontend/android/app/src/main/kotlin/br/com/hematsu/lance_certo/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.hematsu.lance_certo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
