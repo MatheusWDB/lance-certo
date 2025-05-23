2045f8de80882128a8547fa258e588fa3cfad299
MatheusWDB <matheuswdb1703@gmail.com>
2025-05-23 15:26:29 -0300

    build(deps): atualizado o Spring Boot e adicionado dependência do Actuator

--- backend/src/main/java/br/com/hematsu/lance_certo/LanceCertoApplication.java
+++ backend/src/main/java/br/com/hematsu/lance_certo/LanceCertoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LanceCertoApplication {
 
-	public static void main(String[] args) {
+ 	public static void main(String[] args) {
 		SpringApplication.run(LanceCertoApplication.class, args);
 	}
 
