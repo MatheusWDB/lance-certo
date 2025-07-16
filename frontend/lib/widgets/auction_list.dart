4848cd64f954b3716ad662cf430f18ed5b53f784
MatheusWDB <matheuswdb1703@gmail.com>
2025-07-16 15:26:07 -0300

    feat: criado lista de leilões

---
+++ frontend/lib/widgets/auction_list.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AuctionList extends StatefulWidget {
+  const AuctionList({super.key});
+
+  @override
+  State<AuctionList> createState() => _AuctionListItemState();
+}
+
+class _AuctionListItemState extends State<AuctionList> {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Card(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('IMAGEM'),
+            Text('NOME'),
+            Text('DESCRIÇÃO'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(children: [Text('Lance Atual:'), Text('PREÇO')]),
+                Column(children: [Text('Tempo Restante:'), Text('TEMPO')]),
+              ],
+            ),
+            TextButton(onPressed: () {}, child: Text('Ver Detalhes')),
+          ],
+        ),
+      ),
+    );
+  }
+}
