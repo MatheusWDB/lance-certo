31c47126463e5b17222f4093a7b5237eb6cdb188
MatheusWDB <matheuswdb1703@gmail.com>
2025-07-16 20:09:00 -0300

    Revert "Reapply "feat: criado a lógica""
    This reverts commit 426385b59e9d3996ef689f23facb9fb3c10829c6.

--- frontend/lib/screens/login_screen.dart
+++ frontend/lib/screens/login_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:lance_certo/screens/home_screen.dart';
-import 'package:lance_certo/screens/registration_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,15 +8,6 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  Map<String, TextEditingController> controller = {
-    'username': TextEditingController(),
-    'password': TextEditingController(),
-  };
-
-  Map<String, String?> error = {'username': null, 'password': null};
-
-  bool viewPassword = false;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,73 +30,24 @@ class _LoginScreenState extends State<LoginScreen> {
                       Text('Bem-vindo ao Lance Certo'),
                       Text('Entrar'),
                       TextField(
-                        autofocus: true,
-                        controller: controller['username'],
                         decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderSide: const BorderSide(width: 2),
-                            borderRadius: BorderRadius.circular(25),
-                          ),
-                          errorText: error['username'],
+                          border: OutlineInputBorder(),
                           labelText: 'Email',
                         ),
-                        onChanged: (value) {
-                          setState(() {
-                            error['username'] = null;
-                          });
-                        },
                       ),
                       TextField(
-                        autocorrect: false,
-                        controller: controller['password'],
                         decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderSide: const BorderSide(width: 2),
-                            borderRadius: BorderRadius.circular(25),
-                          ),
-                          errorText: error['password'],
+                          border: OutlineInputBorder(),
                           labelText: 'Senha',
-                          suffixIcon: IconButton(
-                            onPressed: () {
-                              setState(() {
-                                viewPassword = !viewPassword;
-                              });
-                            },
-                            icon: Icon(
-                              viewPassword == true
-                                  ? Icons.visibility_off
-                                  : Icons.visibility,
-                            ),
-                          ),
                         ),
-                        obscureText: !viewPassword,
-                        obscuringCharacter: '*',
-                        onChanged: (value) {
-                          setState(() {
-                            error['password'] = null;
-                          });
-                        },
                       ),
-                      ElevatedButton(
-                        onPressed: () => login(),
-                        child: Text('Login'),
-                      ),
+                      ElevatedButton(onPressed: () {}, child: Text('Login')),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Text('Não tem uma conta?'),
                           TextButton(
-                            onPressed: () {
-                              resetController();
-                              resetError();
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                      const RegistrationScreen(),
-                                ),
-                              );
-                            },
+                            onPressed: () {},
                             child: Text('Registre-se aqui'),
                           ),
                         ],
@@ -121,63 +61,5 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
       ),
     );
-  }
-
-  void login() async {
-    final attributes = ['username', 'password'];
-
-    for (var attribute in attributes) {
-      if (controller[attribute]!.text.isEmpty) {
-        setState(() {
-          error[attribute] = 'Campo requerido';
-        });
-        return;
-      }
-    }
-
-    if (!controller['username']!.text.contains('@') ||
-        !controller['username']!.text.contains('.')) {
-      final int atIndex = controller['username']!.text.indexOf('@');
-      final int dotIndex = controller['username']!.text.lastIndexOf('.');
-      if (atIndex < 1 ||
-          dotIndex < atIndex + 2 ||
-          dotIndex == controller['username']!.text.length - 1) {
-        setState(() {
-          error['username'] = 'Inválido';
-        });
-        return;
-      }
-    }
-
-    if (controller['password']!.text.length < 8) {
-      setState(() {
-        error['password'] = 'A senha tem no mínimo 8 caracteres';
-      });
-      return;
-    }
-
-    resetController();
-    resetError();
-
-    if (!mounted) return;
-
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => const HomeScreen()),
-    );
-  }
-
-  void resetController() {
-    setState(() {
-      controller['username']?.clear();
-      controller['password']?.clear();
-    });
-  }
-
-  void resetError() {
-    setState(() {
-      error['username'] = null;
-      error['password'] = null;
-    });
   }
 }
