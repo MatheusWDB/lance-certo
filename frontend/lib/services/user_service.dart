81c9c60e13a8756e8d72bab970d79af16d727ec5
Matheus Wendell Dantas Bezerra <matheuswdb1703@gmail.com>
2025-10-10 17:02:53 -0300

    refactor:

--- frontend/lib/services/user_service.dart
+++ frontend/lib/services/user_service.dart
@@ -6,32 +6,35 @@ import 'package:http/http.dart' as http;
 class UserService {
   static const String baseUrl = 'http://127.0.0.1:8080/api/users';
 
-  static Future<void> registerUser(User user) async {
+  static Future<void> registerUser(User user, String password) async {
     final response = await http.post(
       Uri.parse('$baseUrl/register'),
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(user.toJson(null, null, null)),
+      body: jsonEncode(user.toJson(null, password, null)),
     );
 
     if (response.statusCode != 201) {
-      throw Exception('Erro ao criar usuário: ${response.statusCode}');
+      final data = jsonDecode(response.body);
+      throw Exception('Erro ao criar usuário: ${data['message']}');
     }
   }
 
-  static Future<void> login(User user, String login) async {
+  static Future<void> login(String login, String password) async {
+    final User user = User();
     final response = await http.post(
       Uri.parse('$baseUrl/login'),
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(user.toJson(login, null, null)),
+      body: jsonEncode(user.toJson(login, password, null)),
     );
 
+    final data = jsonDecode(response.body);
     if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
       final Map<String, dynamic> userMap = data['userDTO'];
       final loggedUser = User.fromJson(userMap);
+      loggedUser.token = data['token'];
       User.currentUser = loggedUser;
     } else {
-      throw Exception('Erro ao buscar usuário: ${response.statusCode}');
+      throw Exception('Erro ao buscar usuário: ${data['message']}');
     }
   }
 
