2e1dbd5dcf7054963ad2ad2a4beec7047a110c17
Matheus Wendell Dantas Bezerra <matheuswdb1703@gmail.com>
2025-10-10 16:56:55 -0300

    refactor:

--- backend/src/main/java/br/com/hematsu/lance_certo/LanceCertoApplication.java
+++ backend/src/main/java/br/com/hematsu/lance_certo/LanceCertoApplication.java
@@ -2,13 +2,16 @@ package br.com.hematsu.lance_certo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class LanceCertoApplication {
 
- 	public static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(LanceCertoApplication.class, args);
 	}
 
