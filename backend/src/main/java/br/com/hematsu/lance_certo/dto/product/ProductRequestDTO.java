0131d0790ae4acfbcb2c443d8ddab6cc94d5cd61
Matheus Wendell Dantas Bezerra <matheuswdb1703@gmail.com>
2025-10-29 22:37:57 -0300

    style:

--- backend/src/main/java/br/com/hematsu/lance_certo/dto/product/ProductRequestDTO.java
+++ backend/src/main/java/br/com/hematsu/lance_certo/dto/product/ProductRequestDTO.java
@@ -4,10 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 
 public record ProductRequestDTO(
                 @NotBlank(message = "Product name é obrigatório!") String name,
-
                 @NotBlank(message = "Product description é obrigatório!") String description,
-
                 String imageUrl,
-
                 @NotBlank(message = "Product category é obrigatório!") String category) {
 }
